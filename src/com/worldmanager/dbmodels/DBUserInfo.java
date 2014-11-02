932262974217fd3dab62c0d124c0b9a08cb9828c
zkocen <zkocen@gmail.com>
2014-11-02 20:54:19 +0000

    dbuserinfo

---
+++ src/com/worldmanager/dbmodels/DBUserInfo.java
@@ -0,0 +1,40 @@
+package com.worldmanager.dbmodels;
+
+import org.apache.catalina.tribes.util.UUIDGenerator;
+
+public abstract class DBUserInfo {
+
+	private String uid;
+	private String pwd;
+	private String cat;
+	
+	public DBUserInfo(String userID, String password, String catalog) {
+		uid = userID;
+		pwd = password;
+		cat = catalog;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getCat() {
+		return cat;
+	}
+
+	public void setCat(String cat) {
+		this.cat = cat;
+	}
+}
