6cbe491a7566e36ed515ccc13996b7912e0674e2
zkocen <zkocen@gmail.com>
2014-11-04 23:22:01 +0000

    broken

---
+++ src/com/worldmanager/dbhelpers/DBManagerSetup.java
@@ -0,0 +1,37 @@
+package com.worldmanager.dbhelpers;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import com.worldmanager.dbmodels.DBManager;
+import com.worldmanager.dbmodels.MySQLServerConnectionBehavior;
+import com.worldmanager.dbmodels.ServerConnectionBehaviour;
+
+@WebListener
+public class DBManagerSetup implements ServletContextListener {
+	private DBManager dbm = null;
+	
+    public DBManagerSetup() {
+    }
+
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+    	if (dbm != null) {
+			if (dbm.isConnected()) {
+				dbm.closeConnection(false);
+			}
+		}
+    	dbm = null;
+    }
+
+    public void contextInitialized(ServletContextEvent sce)  { 
+    	ServletContext sc = sce.getServletContext();
+    	String uid = sc.getInitParameter("dbuserid");
+    	String pwd = sc.getInitParameter("dbuserpwd");
+    	String cat = sc.getInitParameter("dbinitcat");
+    	
+    	ServerConnectionBehaviour scb = new MySQLServerConnectionBehavior(uid, pwd, cat);
+    }
+	
+}
---
+++ src/com/worldmanager/models/MySQLServerConnectionBehavior.java
@@ -0,0 +1,9 @@
+package com.worldmanager.models;
+
+import com.worldmanager.dbmodels.DBUserInfo;
+import com.worldmanager.dbmodels.ServerConnectionBehaviour;
+
+public class MySQLServerConnectionBehavior extends DBUserInfo implements
+		ServerConnectionBehaviour {
+
+}
