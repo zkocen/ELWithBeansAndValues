65b735ba0520e97f7553066a4dd715dbde214694
zkocen <zkocen@gmail.com>
2014-11-06 20:15:18 +0000

    moved myswlserverconnectionbehaviot

---
+++ src/com/worldmanager/dbmodels/MySQLServerConnectionBehavior.java
@@ -0,0 +1,14 @@
+package com.worldmanager.dbmodels;
+
+
+public class MySQLServerConnectionBehavior extends DBUserInfo implements ServerConnectionBehaviour {
+	
+	public MySQLServerConnectionBehavior() {
+		super();
+	}
+	
+	public MySQLServerConnectionBehavior(String uid, String pwd, String cat) {
+		
+	}
+	
+}
--- src/com/worldmanager/models/MySQLServerConnectionBehavior.java
+++
@@ -1,9 +0,0 @@
-package com.worldmanager.models;
-
-import com.worldmanager.dbmodels.DBUserInfo;
-import com.worldmanager.dbmodels.ServerConnectionBehaviour;
-
-public class MySQLServerConnectionBehavior extends DBUserInfo implements
-		ServerConnectionBehaviour {
-
-}
