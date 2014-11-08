1d0b2639ddd0e8718eed0339f03cfe4dddafde26
zkocen <zkocen@gmail.com>
2014-11-08 14:18:37 +0000

    testMutatorsAndAccessors

--- src/com/worldmanager/models/Country.java
+++ src/com/worldmanager/models/Country.java
@@ -5,8 +5,20 @@ import java.io.Serializable;
 public class Country implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private String code;
-	
-	
+	private String name;
+	private String continent;
+	private String region;
+	private double surfaceArea;
+	private int indepYear;
+	private int population;
+	private int lifeExpectancy;
+	private double gnp; 
+	private double gnpOld; 
+	private String localName;
+	private String governmentForm;
+	private String headOfState;
+	private String Capital;
+	private String code2;
 	
 	public void setCode(String value) {
 		code = value;
@@ -17,5 +29,170 @@ public class Country implements Serializable {
 	public String getCode() {
 		return code;
 	}
+
+
+
+	public void setName(String value) {
+		name = value;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setContinent(String value) {
+		continent = value;
+	}
+
+
+
+	public String getContinent() {
+		return continent;
+	}
+
+
+
+	public void setRegion(String value) {
+		region = value;
+	}
+
+
+
+	public String getRegion() {
+		return region;
+	}
+
+
+
+	public void setSurfaceArea(double value) {
+		surfaceArea = value;
+	}
+
+
+
+	public double getSurfaceArea() {
+		return surfaceArea;
+	}
+
+
+
+	public void setIndepYear(int value) {
+		indepYear = value;
+	}
+
+
+
+	public int getIndepYear() {
+		return indepYear;
+	}
+
+
+
+	public void setPopulation(int value) {
+		population = value;
+	}
+
+
+
+	public int getPopulation() {
+		return population;
+	}
+
+
+
+	public void setLifeExpectancy(double value) {
+		lifeExpectancy = (int) value;
+	}
+
+
+
+	public int getLifeExpectancy() {
+		return lifeExpectancy;
+	}
+
+
+
+	public void setGnp(double value) {
+		gnp = value;
+	}
+
+
+
+	public double getGnp() {
+		return gnp;
+	}
 	
+	public void setGnpOld(double value) {
+		gnpOld = value;
+	}
+
+
+
+	public double getGnpOld() {
+		return gnpOld;
+	}
+
+
+
+	public void setLocalName(String value) {
+		localName = value;
+	}
+
+
+
+	public String getLocalName() {
+		return localName;
+	}
+
+
+
+	public String getGovernmentForm() {
+		return governmentForm;
+	}
+
+
+
+	public void setGovernmentForm(String value) {
+		governmentForm = value;
+	}
+
+
+
+	public String getHeadOfState() {
+		return headOfState;
+	}
+
+
+
+	public void setHeadOfState(String value) {
+		headOfState = value;
+	}
+
+
+
+	public String getCapital() {
+		return Capital;
+	}
+
+
+
+	public void setCapital(String value) {
+		Capital = value;
+	}
+
+
+
+	public String getCode2() {
+		return code2;
+	}
+
+
+
+	public void setCode2(String value) {
+		code2 = value;
+	}
 }
--- src/com/worldmanager/models/test/CountryTest.java
+++ src/com/worldmanager/models/test/CountryTest.java
@@ -37,6 +37,48 @@ public class CountryTest {
 		c = new Country();
 		c.setCode(code);
 		assertEquals("Could not set code as expected", code, c.getCode());
+		
+		c.setName(name);
+		assertEquals("Could not set name as expected", name, c.getName());
+		
+		c.setContinent(continent);
+		assertEquals("Could not set Continent as expected", continent, c.getContinent());
+		
+		c.setRegion(region);
+		assertEquals("Could not set Region as expected", region, c.getRegion());
+		
+		c.setSurfaceArea(surfaceArea);
+		assertEquals("Could not set surfaceArea as expected", surfaceArea, c.getSurfaceArea(), delta);
+		
+		c.setIndepYear(indepYear);
+		assertEquals("Could not set indepYear as expected", indepYear, c.getIndepYear());
+		
+		c.setPopulation(population);
+		assertEquals("Could not set population as expected", population, c.getPopulation());
+		
+		c.setLifeExpectancy(lifeExpectancy);
+		assertEquals("Could not set lifeExpectancy as expected", lifeExpectancy, c.getLifeExpectancy(), delta);
+		
+		c.setGnp(gnp);
+		assertEquals("Could not set GNP as expected", gnp, c.getGnp(), delta);
+		
+		c.setGnpOld(gnpOld);
+		assertEquals("Could not set GNPOld as expected", gnpOld, c.getGnpOld(), delta);
+		
+		c.setLocalName(localName);
+		assertEquals("Could not set localName as expected", localName, c.getLocalName());
+		
+		c.setGovernmentForm(governmentForm);
+		assertEquals("Could not set governmentForm as expected", governmentForm, c.getGovernmentForm());
+		
+		c.setHeadOfState(headOfState);
+		assertEquals("Could not set headOfState as expected", headOfState, c.getHeadOfState());
+		
+		c.setCapital(Capital);
+		assertEquals("Could not set Capital as expected", Capital, c.getCapital());
+		
+		c.setCode2(code2);
+		assertEquals("Could not set code2 as expected", code2, c.getCode2());
 	}
 
 }
