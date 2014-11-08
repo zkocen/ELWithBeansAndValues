15159a3035ba952eb0c9a824a09cb33f1423f45f
zkocen <zkocen@gmail.com>
2014-11-08 13:34:41 +0000

    country test defult const

--- src/com/worldmanager/models/test/CountryTest.java
+++ src/com/worldmanager/models/test/CountryTest.java
@@ -4,11 +4,32 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.worldmanager.models.Country;
+
 public class CountryTest {
 
+	Country c;
+	private String code = "SLO";
+	private String name = "Slovenia";
+	private String continent = "Europe";
+	private String region = "Koroska";
+	private double surfaceArea = 20.273;
+	private int indepYear = 1991;
+	private int population = 2000000;
+	private double lifeExpectancy = 76;
+	private double gnp = 58509000000.0;
+	private double gnpOld = 43509000000.0;
+	private String localName = "Slovenija";
+	private String governmentForm = "Democracy";
+	private String headOfState = "Miro Cerar";
+	private String Capital = "Ljubljana";
+	private String code2 = "1000";
+	private double delta = 0.0001;
+			
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDefaultConstructor() {
+		c = new Country();
+		assertNotNull("Could not create Country", c);
 	}
 
 }
