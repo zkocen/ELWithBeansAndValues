aa83399d44f4ea2e22e82dd6ffeaf49ea3bd45c8
zkocen <zkocen@gmail.com>
2014-11-08 13:43:04 +0000

    setCode test green

--- src/com/worldmanager/models/Country.java
+++ src/com/worldmanager/models/Country.java
@@ -4,5 +4,18 @@ import java.io.Serializable;
 
 public class Country implements Serializable {
 	private static final long serialVersionUID = 1L;
+	private String code;
+	
+	
+	
+	public void setCode(String value) {
+		code = value;
+	}
+
+
+
+	public String getCode() {
+		return code;
+	}
 	
 }
--- src/com/worldmanager/models/test/CountryTest.java
+++ src/com/worldmanager/models/test/CountryTest.java
@@ -31,5 +31,12 @@ public class CountryTest {
 		c = new Country();
 		assertNotNull("Could not create Country", c);
 	}
+	
+	@Test
+	public void testMutatorsAndAccessors() {
+		c = new Country();
+		c.setCode(code);
+		assertEquals("Could not set code as expected", code, c.getCode());
+	}
 
 }
